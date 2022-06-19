6591d965cbe1a62608e79764f467c4139b52a31d
kotanamy <donat0009@outlook.com>
2022-06-19 17:16:10 +0300

    init

---
+++ swiftuitutorial1/swiftuitutorial1/swiftuitutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuitutorial1App.swift
+//  swiftuitutorial1
+//
+//  Created by Ярослав Котов on 19.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftuitutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
