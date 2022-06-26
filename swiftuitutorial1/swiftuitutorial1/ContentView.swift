17fc2a1a0f830c9a9a548f9e60944a7c1fe2a298
kotanamy <donat0009@outlook.com>
2022-06-26 14:43:03 +0300

    Работа с изображением и градиент + @State + Toggle + Button

--- swiftuitutorial1/swiftuitutorial1/ContentView.swift
+++ swiftuitutorial1/swiftuitutorial1/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 import UIKit
 
+//Основы
+// VStack
+// HStack
+// ZStack
 // body - обязательное вычисляемое свойство
 struct ContentView: View {
     var body: some View {
@@ -37,6 +41,8 @@ struct ContentView: View {
     }
 }
 
+
+// Работа с текстом
 struct ContentView2Text: View {
     var body: some View {
         VStack {
@@ -44,7 +50,7 @@ struct ContentView2Text: View {
                 .lineLimit(1) // Принудительно сжать до 1 строки
                 .truncationMode(.middle) // сокращаем то что в середине
                 .font(.headline)
-                .background(Color.init(uiColor: UIColor.green))
+                .background(Color.init(.link))
             
             Text("AAAAAAAAAAAAAAA A AAAIUDAGDIAD H DioahDioAHDOHADO IODAHiodhqp-ufhnsloag1")
                 .kerning(5) // между букв добавляет растояние в конце, если не нужно растояние в конце, то используйте tracking(5)
@@ -66,13 +72,94 @@ struct ContentView2Text: View {
     }
 }
 
+// Работа с изображением и градиент
+struct ContentView3Image: View {
+    var body: some View {
+        VStack {
+            VStack{
+            // Можно конвертировать как обычный текст
+            Image(systemName: "cloud.sun.fill") // cистемная картинка (SF Symbols (прога с системными изображениями))
+                .font(.largeTitle)
+                .padding(30)
+                    .background(Color.blue) // может принимать изображение в параметры
+                    .foregroundColor(.white)
+                    .clipShape(Circle()) // накладываем форму (круга) (Capsule\RoundedRectangle ...)
+                // .flame(width: 200, height: 200) - рамка
+            // по дефолту растянуто
+            Image("image2")
+                .resizable()                    // оставляет картинку в ее прежнем
+                .aspectRatio(contentMode: .fit) // соотношении сторон
+            }
+        
+        HStack{
+            // Градиент
+            Text("Huhaha")
+                .font(.largeTitle)
+                .padding()
+                .foregroundColor(.white)
+                .background(LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .leading, endPoint: .trailing)) // сверху вниз (.top .bottom)
+        
+        }
+            HStack {
+                // Радиальный градиент
+                let colors = Gradient(colors: [.red, .blue, .green, .white, .purple])
+                // let gradient = RadialGradient(gradient: colors, center: .center, startRadius: 30, endRadius: 200) // из центра наружу
+                let gradient = AngularGradient(gradient: colors, center: .center) // по кругу
+                Circle()
+                    // .fill(gradient)
+                    // .frame(width: 200, height: 200)
+                    .strokeBorder(gradient, lineWidth: 30)
+            }
+        }
+    }
+}
+
+// Взаимодействие с экраном (Binding)
+// @State + Toggle + Button
+struct ContentView4StateToggle: View {
+    
+    //Property Wrapper: @State
+    
+    @State private var showHello = true
+    @State private var showDetail = false // для кнопки
+    
+    var body: some View {
+        VStack {
+            // Toggle(isOn: $showHello, label: {Text("Hello")})
+            // $ - для биндинга нужен
+            // Аналогично переписываем
+            Toggle(isOn: $showHello){ // вынесли замыкание
+                Text("Hello")
+            } .padding()
+            
+            if showHello {
+                Text("Toggle true")
+            }
+            
+            Button(action: {
+                self.showDetail.toggle() // не тот тогл который выше (переключает bool (т.е. showDetail))
+            }, label: {
+                Text("ShowDetails")
+            }).background(.black).padding()
+            
+            if showDetail {
+                Text("Some details here...")
+            }
+        }
+    }
+}
+
+
+
 // Нужна для отображения в canvas xCode
 // В продакт не идет
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             // ContentView()
-            ContentView2Text()
+            // ContentView2Text()
+            // ContentView3Image()
+            ContentView4StateToggle()
         }
     }
 }
